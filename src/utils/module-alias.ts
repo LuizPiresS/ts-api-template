c01f95fb40db3ce310329e38190d7df651eed786
Luiz Pires <kw13code@gmail.com>
2020-09-13 13:44:32 +0000

    refactor: remove test path

--- src/utils/module-alias.ts
+++ src/utils/module-alias.ts
@@ -1,9 +1,8 @@
-import * as path from 'path';
 import moduleAlias from 'module-alias';
+import * as path from 'path';
 
 const files = path.resolve(__dirname, '../..');
 
 moduleAlias.addAliases({
   '@src': path.join(files, 'src'),
-  '@test': path.join(files, 'test'),
 });
